4103a69f696632cc7ff75f1de242eb483a4c65c2
Furkan_Onur <onurtozlu@hotmail.com>
2024-08-05 20:13:30 +0300

    test

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ document.addEventListener('scroll', ()=> {
         nav.classList.remove('scrolled');
     }
 })
+
+document.addEventListener('scroll',() => {
+    const grid = document.getElementById("grid-template-box");
+    const gridpass = document.getElementById("grid-template-box");
+
+    if (window.scrollY >  gridpass.offsetHeight) {
+        grid.classList.add('scrolled');
+    }
+    else {
+        grid.classList.remove('scrolled');
+    }
+})
