d1f998786d34d4b5eda773b4963ced097d82269a
Furkan_Onur <onurtozlu@hotmail.com>
2024-08-07 00:29:20 +0300

    fixes

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 document.addEventListener('scroll', () => {
     const nav = document.getElementById("nav");
     const aside = document.getElementById('set-height');
+    const artheader = document.getElementById('artisth1');
     const gridBoxes = document.querySelectorAll('.grid-template-box');
     const gridBoxes2 = document.querySelectorAll('.grid-template-box-2');
     const gridBoxes3 = document.querySelectorAll('.grid-template-box-3');
+    const gridBoxes4 = document.querySelectorAll('.grid-template-box-4');
 
     if (window.scrollY > aside.offsetHeight) {
         nav.classList.add('scrolled');
     } else {
         nav.classList.remove('scrolled');
+    }
+    if (window.scrollY > aside.offsetHeight) {
+        artheader.classList.add('aı');
+    } else {
+        artheader.classList.remove('aı');
     }
 
     gridBoxes.forEach(gridBox => {
@@ -32,6 +39,13 @@ document.addEventListener('scroll', () => {
             gridBox.classList.add('scrolled3');
         } else {
             gridBox.classList.remove('scrolled3');
+        }
+    });
+    gridBoxes4.forEach(gridBox => {
+        if (window.scrollY > aside.offsetHeight) {
+            gridBox.classList.add('scrolled4');
+        } else {
+            gridBox.classList.remove('scrolled4');
         }
     });
 
